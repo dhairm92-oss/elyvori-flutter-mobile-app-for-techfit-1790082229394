025419de214c7148257a211e38f63a15b5bd84d4
dhairm92-oss <dhairm92@gmail.com>
2026-09-22 16:04:06 +0300

    Add lib/widgets/techfit_bottom_nav.dart

---
+++ lib/widgets/techfit_bottom_nav.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+class TechFitBottomNav extends StatelessWidget {
+  final int currentIndex;
+  final ValueChanged<int> onTap;
+
+  const TechFitBottomNav({
+    Key? key,
+    required this.currentIndex,
+    required this.onTap,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return BottomNavigationBar(
+      currentIndex: currentIndex,
+      onTap: onTap,
+      backgroundColor: const Color(0xFF1E1E2C),
+      selectedItemColor: const Color(0xFF00E676),
+      unselectedItemColor: Colors.grey,
+      type: BottomNavigationBarType.fixed,
+      items: const [
+        BottomNavigationBarItem(
+          icon: Icon(Icons.qr_code_scanner),
+          label: 'Check-In',
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.history),
+          label: 'History',
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.person),
+          label: 'Profile',
+        ),
+      ],
+    );
+  }
+}
