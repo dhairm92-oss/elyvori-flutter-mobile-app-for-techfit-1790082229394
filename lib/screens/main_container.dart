d74d06eddf86c7d4f56ea154250a3037aa489546
dhairm92-oss <dhairm92@gmail.com>
2026-09-22 16:04:02 +0300

    Add lib/screens/main_container.dart

---
+++ lib/screens/main_container.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import '../widgets/techfit_bottom_nav.dart';
+import 'check_in_screen.dart';
+import 'history_screen.dart';
+import 'profile_screen.dart';
+
+class MainContainer extends StatefulWidget {
+  const MainContainer({Key? key}) : super(key: key);
+
+  @override
+  State<MainContainer> createState() => _MainContainerState();
+}
+
+class _MainContainerState extends State<MainContainer> {
+  int _currentIndex = 0;
+
+  final List<Widget> _screens = const [
+    CheckInScreen(),
+    HistoryScreen(),
+    ProfileScreen(),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: IndexedStack(
+        index: _currentIndex,
+        children: _screens,
+      ),
+      bottomNavigationBar: TechFitBottomNav(
+        currentIndex: _currentIndex,
+        onTap: (index) {
+          setState(() {
+            _currentIndex = index;
+          });
+        },
+      ),
+    );
+  }
+}
